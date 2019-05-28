6a458069c3f2d8eb14aec06bf11ac1a25379a159
Headless Granules <hgranule@at-i4.21-school.ru>
2019-05-28 11:26:02 +0300

    dlstshift fix

--- srcs/ft_dlstshift.c
+++ srcs/ft_dlstshift.c
@@ -6,7 +6,7 @@
 /*   By: hgranule <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 08:31:05 by hgranule          #+#    #+#             */
-/*   Updated: 2019/05/28 08:41:08 by hgranule         ###   ########.fr       */
+/*   Updated: 2019/05/28 11:25:40 by hgranule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ t_dlist		*ft_dlstshift(t_dlist **list)
 	if (!list || !(*list))
 		return (0);
 	if ((lst = *list)->next)
-	{
-		*list = lst->next;
 		lst->next->prev = 0;
-	}
+	*list = lst->next;
 	return (lst);
 }
