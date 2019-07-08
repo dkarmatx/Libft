ee0faf3126db1b76599e29affee8f6a18a47c9d5
Headless Granules <hgranule@at-b1.21-school.ru>
2019-07-08 06:01:20 +0300

    leak fix strsub

--- srcs/ft_strsub.c
+++ srcs/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: hgranule <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 06:05:59 by hgranule          #+#    #+#             */
-/*   Updated: 2019/05/03 06:34:50 by hgranule         ###   ########.fr       */
+/*   Updated: 2019/07/08 02:25:09 by hgranule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char			*substr;
 
-	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (s)
+	{
+		substr = (char *)malloc(sizeof(char) * (len + 1));
 		if (substr)
 		{
 			ft_memcpy(substr, (s + start), len);
 			substr[len] = 0;
 			return (substr);
 		}
+	}
 	return (0);
 }
